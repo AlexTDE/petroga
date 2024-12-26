fe10677c981d601fa427804415b4c5ffdf7cd76c
Alena <96665824+AlexTDE@users.noreply.github.com>
2024-12-26 21:21:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, render_template, jsonify
+import requests
 
+BOT_TOKEN = '7169416179:AAHdY_lwlNpGkQyCL8xj7WJIh_rgUqfOlUw'
+CHAT_ID = '-1002056665555'
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,8 +14,12 @@ def index():
             return jsonify({"error": "Нет ответа"}), 400
         message = request.form.get('message')
         # Сохраняем ответ в файл
-        with open("answers.txt", "a", encoding='utf-8') as file:  # Открываем файл в режиме добавления
-            file.write(f"{answer},{message}\n")  # Сохраняем ответ с новой строки
+        # Отправка сообщения в Telegram
+        text = f"Option: {answer}\nСообщение: {message}"
+        requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", params={
+            "chat_id": CHAT_ID,
+            "text": text
+        })
         return jsonify({"message": "Ответ сохранен!"}), 200
     return render_template('index.html')  # Указываем файл index.html
 
